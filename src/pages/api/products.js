1cd3d810d4cbb2d3906c8af55a456ad26aada169
PashmeenZia <pashmeenzia@gmail.com>
2025-01-08 16:54:47 +0500

    initial commit

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
         name: " Veg Beef Boti",
         price: 10,
         image:
-          "image/31.jpg",
+          "/image/31.jpg",
       },
   
       {
@@ -14,34 +14,34 @@ export default function handler(req, res) {
         name: "Chicken Biryani",
         price: 12,
         image:
-          "image/8.jpg",
+          "/image/8.jpg",
       },
       {
         id: 3,
         name: "Chiken Tikka",
         price: 8,
         image:
-          "image/28.jpg",
+          "/image/28.jpg",
       },
       {
         id: 4,
         name: "Chicken Karhai",
         price: 15,
         image:
-          "image/4.jpg",
+          "/image/4.jpg",
       },
       {
         id: 5,
         name: "Fish Fry",
         price: 13,
-        image: "image/29.jpg",
+        image: "/image/29.jpg",
       },
       {
         id: 6,
         name: "Prawn",
         price: 14,
         image:
-          "image/30.jpg",
+          "/image/30.jpg",
       },
       
     ];
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export interface ProductData {
+export interface Product {
     id: number;
     name: string;
     price: number;
